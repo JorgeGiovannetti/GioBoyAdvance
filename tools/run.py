88c2e790b1d324dfa42003e259cebeb0df862f12
JorgeGiovannetti <giovannetti2000@gmail.com>
2021-10-12 17:22:03 -0500

    Added run script

---
+++ tools/run.py
@@ -0,0 +1,20 @@
+import os, subprocess, sys
+import globals
+
+# Run project
+
+def findExecutable(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+
+if globals.IsWindows():
+    ret = 0
+    if not os.path.exists(os.getcwd() + '/bin'):
+        print('Building project...')
+        ret = subprocess.call('python tools\\build.py')
+    executable = findExecutable('GioBoyAdvance.exe', os.getcwd() + '/bin')
+    if executable is not None:
+        ret = subprocess.call(executable)
+
+sys.exit(ret)
