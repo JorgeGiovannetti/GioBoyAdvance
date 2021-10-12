06fce5b6bab4998d186fea7a8504f13b732743bf
JorgeGiovannetti <giovannetti2000@gmail.com>
2021-10-12 00:54:34 -0500

    Added clean tool script

---
+++ tools/clean.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+
+def removeFile(filename):    
+    try:
+        os.remove(filename)
+    except OSError as e:
+        print ("Error: %s - %s." % (e.filename, e.strerror))
+
+def removeFolder(dirname):
+    try:
+        shutil.rmtree(dirname)
+    except OSError as e:
+        print ("Error: %s - %s." % (e.filename, e.strerror))
+
+files = ['GioBoyAdvance.sln', 'GioBoyAdvance.vcxproj']
+dirs = ['bin', 'bin-obj']
+
+for f in files:
+    removeFile(f)
+
+for d in dirs:
+    removeFolder(d)
