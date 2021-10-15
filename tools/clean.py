055dae707b20599505c0340977919e4d798d486b
JorgeGiovannetti <giovannetti2000@gmail.com>
2021-10-14 19:42:06 -0500

    Removed error spam when file not found

--- tools/clean.py
+++ tools/clean.py
@@ -5,15 +5,17 @@ import shutil
 def removeFile(filename):
     try:
         os.remove(filename)
+        return "Removed file: %s" % (filename)
     except OSError as e:
-        print("Error: %s - %s." % (e.filename, e.strerror))
+        return "Error: %s - %s." % (e.filename, e.strerror)
 
 
 def removeFolder(dirname):
     try:
         shutil.rmtree(dirname)
+        return "Removed dir: %s" % (dirname)
     except OSError as e:
-        print("Error: %s - %s." % (e.filename, e.strerror))
+        return "Error: %s - %s." % (e.filename, e.strerror)
 
 
 files = [
@@ -31,8 +33,18 @@ dirs = [
     'vendor/glad/bin-obj'
 ]
 
+filesDeleted = 0
+
 for f in files:
-    removeFile(f)
+    res = removeFile(f)
+    if res[:5] != "Error":
+        filesDeleted += 1
+
+dirsDeleted = 0
 
 for d in dirs:
     removeFolder(d)
+    if res[:5] != "Error":
+        dirsDeleted += 1
+
+print('Cleaned up %s files and %s directories' % (filesDeleted, dirsDeleted))
