6cd64a1f6fe122b44a02643c9e8e45c163c42427
JorgeGiovannetti <giovannetti2000@gmail.com>
2021-10-21 22:58:17 -0500

    Removed illegal log macros

--- include/platform/core/log.h
+++ include/platform/core/log.h
@@ -23,11 +23,9 @@ namespace platform::core
 #define CLIENT_LOG_INFO(...) ::platform::core::Log::GetClientLogger()->info(__VA_ARGS__)
 #define CLIENT_LOG_WARN(...) ::platform::core::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define CLIENT_LOG_ERROR(...) ::platform::core::Log::GetClientLogger()->error(__VA_ARGS__)
-#define CLIENT_LOG_FATAL(...) ::platform::core::Log::GetClientLogger()->fatal(__VA_ARGS__)
 
 // GBA log macros
 #define GBA_LOG_TRACE(...) ::platform::core::Log::GetGBALogger()->trace(__VA_ARGS__)
 #define GBA_LOG_INFO(...) ::platform::core::Log::GetGBALogger()->info(__VA_ARGS__)
 #define GBA_LOG_WARN(...) ::platform::core::Log::GetGBALogger()->warn(__VA_ARGS__)
 #define GBA_LOG_ERROR(...) ::platform::core::Log::GetGBALogger()->error(__VA_ARGS__)
-#define GBA_LOG_FATAL(...) ::platform::core::Log::GetGBALogger()->fatal(__VA_ARGS__)
